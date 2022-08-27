0818ca9036dde8f5b97275df30dd7450dc18fe2f
Alissa <alissamak@outlook.com>
2022-08-27 15:18:51 -0400

    added in form and pets show pages, new routes and updated json file

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,17 +1,22 @@
-# config
-from flask import Flask 
+# config                    
+from flask import Flask
 
 # factory
-def create_app(): 
+def create_app():
     app = Flask(__name__)
 
     # index route
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
 
-    # register pet blueprint
-    from . import pet
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
 
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
     return app
