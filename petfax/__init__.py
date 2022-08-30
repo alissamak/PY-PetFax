cb8897ad0b4cd8d1ed34ff11823c805f65f21227
Alissa <alissamak@outlook.com>
2022-08-29 21:36:09 -0400

    added in new facts/index.html and models to connect to sql database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,9 +1,21 @@
 # config                    
 from flask import Flask
+from flask_migrate import Migrate
 
 # factory
 def create_app():
     app = Flask(__name__)
+
+    #database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Jackson123!@localhost:5432/petfax"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #import models db
+    from . import models
+    models.db.init_app(app)
+
+    #Flask migration from models to db
+    migrate = Migrate(app, models.db)
 
     # index route
     @app.route('/')
